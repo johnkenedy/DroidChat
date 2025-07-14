173f47b947b0d71872fc2922cf59a64581d5f3ab
John Kenedy <john.kenedy0410@gmail.com>
2025-07-14 09:29:11 -0300

    Create FormValidator to the SignUpScreen.kt

---
+++ app/src/main/kotlin/br/com/ada/droidchat/ui/feature/signup/SignUpFormValidator.kt
@@ -0,0 +1,35 @@
+package br.com.ada.droidchat.ui.feature.signup
+
+import br.com.ada.droidchat.R
+import br.com.ada.droidchat.ui.validator.EmailValidator
+import br.com.ada.droidchat.ui.validator.FormValidator
+import br.com.ada.droidchat.ui.validator.PasswordValidator
+
+class SignUpFormValidator : FormValidator<SignUpFormState> {
+
+    override fun validate(formState: SignUpFormState): SignUpFormState {
+        val isFirstNameValid = formState.firstName.isNotEmpty() && !formState.firstName.any { it.isDigit() } && formState.firstName.length > 2
+        val isLastNameValid = formState.lastName.isNotEmpty() && !formState.lastName.any { it.isDigit() } && formState.lastName.length > 2
+        val isEmailValid = EmailValidator.isValid(formState.email)
+        val isPasswordValid = PasswordValidator.isValid(formState.password)
+        val isConfirmPasswordValid = PasswordValidator.isValid(formState.confirmPassword)
+                && formState.password == formState.confirmPassword
+        val hasError = listOf(
+            isFirstNameValid,
+            isLastNameValid,
+            isEmailValid,
+            isPasswordValid,
+            isConfirmPasswordValid
+        ).any { !it }
+
+        return formState.copy(
+            firstNameError = if (!isFirstNameValid) R.string.error_message_field_blank else null,
+            lastNameError = if (!isLastNameValid) R.string.error_message_field_blank else null,
+            emailError = if (!isEmailValid) R.string.error_message_email_invalid else null,
+            passwordError = if (!isPasswordValid) R.string.error_message_password_invalid else null,
+            confirmPasswordError = if (!isConfirmPasswordValid) R.string.error_message_password_confirmation_invalid else null,
+            hasError = hasError,
+        )
+    }
+
+}
---
+++ app/src/main/kotlin/br/com/ada/droidchat/ui/validator/PasswordValidator.kt
@@ -0,0 +1,13 @@
+package br.com.ada.droidchat.ui.validator
+
+object PasswordValidator {
+
+    fun isValid(value: String): Boolean {
+        return value.length >= 8 &&
+                value.any { it.isUpperCase() } &&
+                value.any { it.isLowerCase() } &&
+                value.any { it.isDigit() } &&
+                value.any { !it.isLetterOrDigit() }
+    }
+
+}
